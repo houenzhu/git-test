bc89622689bcaccc88d3040382a88781c3f4f3e8
houenzhu <houenzhu@qq.com>
2024-08-10 12:22:13 +0800

    第一次提交

---
+++ Zhe-Erp/src/main/java/com/zhe/erp/MainApplication.java
@@ -0,0 +1,12 @@
+package com.zhe.erp;
+
+/**
+ * @version 1.0
+ * @Author 朱厚恩
+ */
+
+public class MainApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
